d8bac1d8202efe90698553d4aaa94d1dcd9524d7
orb <orb@test.com>
2023-04-27 21:51:19 -0500

    Problem 2 - Valid Parentheses

---
+++ src/lc_0002_valid_parentheses.rs
@@ -0,0 +1,45 @@
+pub fn is_valid(s: String) -> bool {
+    let mut stack = "".to_string();
+
+    for ch in s.chars() {
+        match ch {
+            '(' => stack.push(')'),
+            '[' => stack.push(']'),
+            '{' => stack.push('}'),
+            ')' | ']' | '}' => {
+                if let Some(m) = stack.pop() {
+                    if m != ch {
+                        return false
+                    }
+                }
+            }
+            _ => return false
+        }
+    }
+    stack.is_empty()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        assert!(is_valid("()".to_string()))
+    }
+
+    #[test]
+    fn example2() {
+        assert!(is_valid("()[]{}".to_string()))
+    }
+
+    #[test]
+    fn example3() {
+        assert!(!is_valid("(]".to_string()))
+    }
+
+    #[test]
+    fn empty_string() {
+        assert!(is_valid("".to_string()))
+    }
+}
