28f31d65d3a1520bf6fd70923299450d4e85ee32
orb <orb@test.com>
2023-04-27 21:09:30 -0500

    Problem 1 - Two Sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod lc_0001_two_sum;
+pub use lc_0001_two_sum::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod lc_0002_valid_parentheses;
+pub use lc_0002_valid_parentheses::*;
